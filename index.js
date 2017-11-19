0a57edc1cf0764d3f4e54a4f03c8eafe1cdedf61
Faisal Umair <eff.umair@gmail.com>
2017-11-19 23:51:35 +0530

    Preparing for first release

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 500, height: 600, resizable: true})
+  win = new BrowserWindow({width: 500, height: 600, resizable: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
